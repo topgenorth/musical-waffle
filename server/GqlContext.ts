cc391ea2e9c174d68d7b41de2532ac8032de8b86
Tom Opgenorth <tom@opgenorth.net>
2021-05-02 16:22:26 -0600

    WIP

--- server/GqlContext.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import { PubSub } from "apollo-server-express";
-
-export interface GqlContext {
-    req: Request;
-    res: Response;
-    pubsub: PubSub;
-}
